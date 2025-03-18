2f4514ea6d97d8096e8bc715c2c1196d08deeddb
Shweta <shwsrivastava07@gmail.com>
2025-03-18 23:19:52 +0530

    Case Sensitivity resolved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Latest from "../components/latest";
+import Latest from "../components/Latest";
 import Bestseller from "../components/Bestseller";
 import Exchange from "../components/Exchange";
 import Newsletter from "../components/Newsletter";
